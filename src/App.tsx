967be7d83b5b808cdff9e09c3b62a3b29f870488
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-07-25 11:22:05 +0200

    separating sections, installing bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <>
-      <header className="aside">
-        <div>
-          <img className="sidebarimage" src="../public/images/IMG_0013.JPG" />
+      <div className="d-flex flex-row justify-content-center">
+        {/* 01 sidebar */}
+        <div className="aside d-flex flex-column">
+          <Sidebar />
         </div>
-        <div></div>
-      </header>
 
-      <main className="main-wrapper">main portfolio</main>
+        {/* main section */}
+        <div id="technologies" className=" d-flex flex-column main-section">
+          <div className="card">
+            <h1>Viktor Ilie</h1>
+            <h4>Full Stack Developer</h4>
+            <p className="p-5 m-5">
+              I'm a software developer with passion to work on both frontend and
+              backend. Specialized in react and developing my skills in C#. With
+              experience for complex scalable web apps.
+            </p>
+            <p>
+              <b>Want to know how I may help your project?</b>
+            </p>
+            <p>Check out my portfolioand my resume</p>
+            <button className="btn btn-outline-primary">my Resume`</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div className="d-flex justify-content-center align-items-center shadow-lg">
+        <img className="sidebarimage" src="../public/images/IMG_0013.JPG" />
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
