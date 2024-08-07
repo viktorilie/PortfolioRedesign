2990622a9864588a94bb01a35c281c40bf33728d
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-08-07 22:04:50 +0200

    sidenav links

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,37 @@
+import App from "../App";
 import "./Sidebar.css";
 
 const Sidebar = () => {
   return (
     <>
-      <div className="d-flex justify-content-center align-items-center shadow-lg">
-        <img className="sidebarimage" src="../public/images/IMG_0013.JPG" />
+      <div className="d-flex justify-content-center align-items-center ">
+        <img
+          className="sidebarimage shadow-lg"
+          src="../public/images/IMG_0013.JPG"
+        />
       </div>
+      <div className="sidenavlinks">
+        <div className="list-group  list-none">
+          <a
+            href="#"
+            className="list-group-item list-group-item-action list-group-item-secondary">
+            About me
+          </a>
+          <a className="list-group-item list-group-item-action list-group-item-secondary">
+            Skills
+          </a>
+          <a className="list-group-item list-group-item-action list-group-item-secondary">
+            Portflio
+          </a>
+          <a className="list-group-item list-group-item-action list-group-item-secondary">
+            Resume
+          </a>
+          <a className="list-group-item list-group-item-action list-group-item-secondary">
+            Contact me
+          </a>
+        </div>
+      </div>
+      <div className="icon-links"></div>
     </>
   );
 };
