2f6aa8bc30fdea31e5c07b65724e62abcfdf8f9a
Viktor Ilie <viktor.ilie@outlook.com>
2024-10-16 00:20:01 +0200

    main card button change of color

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -17,23 +17,24 @@ const MainCard = () => {
             <p>
               I'm a software developer with passion to work on both frontend and
               backend. Specialized in react and developing my skills in C#. With
-              experience for complex scalable web apps.
+              experience for complex scalable web apps. I'm looking for new and
+              exciting opportunities. Open for remote work.
             </p>
           </div>
-          <div className="py-0 d-flex ">
+          <div className="py-1 d-flex ">
             <p>
               <b>Want to know how I may help your project?</b>
               <br />
               Check out my portfolio and my resume
             </p>
           </div>
-          <div className="align-items-center jus">
+          <div className="align-items-center">
             <button className="btn-portfolio">
               <BsCaretRightFill className="mx-1" />
               View my Portfolio
             </button>
-            <button className="btn-resume">
-              <BsFileEarmarkCode className="mx-2" size={20} />
+            <button className="btn-portfolio">
+              <BsFileEarmarkCode className="mx-1" size={20} />
               View my Resume
             </button>
           </div>
