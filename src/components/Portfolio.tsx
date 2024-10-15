ca2012dd3ee5bffaed8e4d2102d1e2e17714401a
Viktor Ilie <viktor.ilie@outlook.com>
2024-10-15 16:03:58 +0200

    adding portfolio with placeholder and lorem

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,7 +1,69 @@
 const Portfolio = () => {
   return (
-    <div className="porfolio">
-      <div className="card d-flex flex-row container-md"></div>
+    <div className="porfolio container">
+      <div className="card d-flex flex-column m-3 p-5">
+        <h3 className="">Portfolio</h3>
+        <div className="d-flex flex-row pb-5 pt-2 ">
+          <h4>Projects I worked on:</h4>
+        </div>
+
+        {/* // Add a new project here */}
+        <div className="d-flex flex-row py-3">
+          <div className="d-flex flex-column">
+            <img
+              src="https://placehold.co/250"
+              alt="project1"
+              className="img-fluid rounded w-100"
+            />
+          </div>
+          <div className="d-flex flex-column">
+            <h6 className="fw-bold p-2">Project 1</h6>
+            <p className="p-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iusto
+              deserunt, quam, nobis eveniet esse eos et beatae unde sed ex,
+              doloribus at animi architecto! Consequuntur cumque mollitia porro
+              voluptas.
+            </p>
+          </div>
+        </div>
+        <div className="d-flex flex-row py-3">
+          <div className="d-flex flex-column">
+            <img
+              src="https://placehold.co/250"
+              alt="project1"
+              className="img-fluid rounded w-100"
+            />
+          </div>
+          <div className="d-flex flex-column">
+            <h6 className="fw-bold p-2">Project 2</h6>
+            <p className="p-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iusto
+              deserunt, quam, nobis eveniet esse eos et beatae unde sed ex,
+              doloribus at animi architecto! Consequuntur cumque mollitia porro
+              voluptas.
+            </p>
+          </div>
+        </div>
+
+        <div className="d-flex flex-row py-3">
+          <div className="d-flex flex-column">
+            <img
+              src="https://placehold.co/250"
+              alt="project1"
+              className="img-fluid rounded w-100"
+            />
+          </div>
+          <div className="d-flex flex-column">
+            <h6 className="fw-bold p-2">Project 2</h6>
+            <p className="p-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iusto
+              deserunt, quam, nobis eveniet esse eos et beatae unde sed ex,
+              doloribus at animi architecto! Consequuntur cumque mollitia porro
+              voluptas.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
