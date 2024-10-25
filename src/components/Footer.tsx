7bd59298c47062c0efd2c4ab0c75c9ca06a6c8b9
Viktor Ilie <viktor.ilie@outlook.com>
2024-10-25 19:57:45 +0200

    footee font fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <>
-      <footer className="pt-5 pb-0 mb-0 mt-5">
-        <p className="text-center text-black-50">
+      <footer className="pt-5 pb-0 mb-0 mt-5 mx-5 px-5">
+        <p className="text-center text-black-50 footer-text diplay-6">
           inspired by the tech comunity of twitter | daily.dev | github | Viktor
           Ilie &copy; 2024 | All Rights Reserved
         </p>
